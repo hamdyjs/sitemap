2bfab440c9b70e2e637b4cd4a2c0061964a628ff
Ahmed Hamdy <hamdyjs@gmail.com>
2019-01-21 23:53:05 +0200

    Refactor getting links to a separate function

--- main.go
+++ main.go
@@ -12,18 +12,26 @@ func main() {
 	url := flag.String("url", "http://gophercises.com", "The url to build the sitemap of")
 	flag.Parse()
 
-	res, err := http.Get(*url)
+	siteLinks, err := getLinksFromURL(*url)
 	if err != nil {
 		fmt.Println("ERROR:", err)
 		return
 	}
 
-	siteLinks, err := link.Parse(res.Body)
+	fmt.Printf("%+v", siteLinks)
+}
+
+func getLinksFromURL(url string) ([]link.Link, error) {
+	res, err := http.Get(url)
 	if err != nil {
-		fmt.Println("ERROR:", err)
-		return
+		return nil, err
+	}
+
+	links, err := link.Parse(res.Body)
+	if err != nil {
+		return nil, err
 	}
 	res.Body.Close()
 
-	fmt.Printf("%+v", siteLinks)
+	return links, nil
 }
